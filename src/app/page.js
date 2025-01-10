c35a56075a57ae5a714750af022d7f0ff59e3134
Mik <mik23@protonmail.ch>
2025-01-10 14:20:36 +0530

    fixed shadow issues

--- src/app/page.js
+++ src/app/page.js
@@ -8,7 +8,7 @@ import {
   OrbitControls,
   useHelper,
 } from "@react-three/drei";
-import { SpotLightHelper } from "three";
+import { DirectionalLightHelper, SpotLightHelper } from "three";
 // import { useControls } from "leva";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 
@@ -23,7 +23,7 @@ export default function Home() {
   }, []);
 
   return (
-    <div className="flex bg-red-100  justify-center items-center h-screen">
+    <div className="flexjustify-center items-center h-screen">
       <Canvas>
         {/* Gizmo Wrapper */}
         <GizmoHelper alignment="bottom-right" margin={[80, 80]}>
@@ -32,11 +32,82 @@ export default function Home() {
         </GizmoHelper>
         <gridHelper args={[20, 20, 0xff22aa, 0x55ccff]} />
         {/* <AnimatedBox /> */}
-        <TShirt />
-        <SpotLightWithHelper />
+        <TShirt fileUrl="/untitled.glb" scale={10} />
+        {/* <SpotLightWithHelper
+          intensity={200}
+          position={[1, 5, 0]}
+          angle={Math.PI / 8}
+        /> */}
+        {/* <ambientLight color={0xfcfcfc} /> */}
+        <DirectionalLightWithHelper
+          color="0x000"
+          intensity={50}
+          position={[1, 2, 10]}
+        />
+
         <OrbitControls />
       </Canvas>
     </div>
+  );
+}
+
+function DirectionalLightWithHelper({
+  intensity,
+  position,
+  angle = 0,
+  color = "0xff22aa",
+}) {
+  const light = useRef();
+  useHelper(light, DirectionalLightHelper, 2, "crimson");
+
+  return (
+    <directionalLight
+      ref={light}
+      intensity={intensity}
+      position={position}
+      angle={angle}
+      color={color}
+      castShadow
+    />
+  );
+}
+
+function SpotLightWithHelper({
+  intensity,
+  position,
+  angle = 0,
+  color = "0x000",
+}) {
+  const light = useRef();
+
+  useHelper(light, SpotLightHelper, "orange");
+
+  return (
+    <spotLight
+      ref={light}
+      intensity={intensity}
+      position={position}
+      angle={angle}
+      color={color}
+    />
+  );
+}
+
+function TShirt({ fileUrl = "untitled.glb", position = [0, 0, 0] }, scale = 1) {
+  const mesh = useRef(null);
+  const gltf = useLoader(GLTFLoader, fileUrl);
+
+  // Re-add the useFrame hook to rotate the object
+  useFrame(() => {
+    if (mesh.current) {
+      mesh.current.rotation.y += 0.01;
+    }
+  });
+
+  return (
+    <mesh ref={mesh} scale={scale}>
+      <primitive object={gltf.scene} position={position} />
+    </mesh>
   );
 }
 
@@ -61,39 +132,6 @@ function AnimatedBox() {
     <mesh ref={boxRef}>
       <boxGeometry args={[2, 3, 2]} />
       <meshPhongMaterial color={0x00bfff} />
-    </mesh>
-  );
-}
-
-function SpotLightWithHelper() {
-  const light = useRef();
-
-  useHelper(light, SpotLightHelper, "orange");
-
-  return (
-    <spotLight
-      ref={light}
-      intensity={50}
-      position={[4, 2, 3]}
-      angle={Math.PI / 8}
-    />
-  );
-}
-
-function TShirt({ fileUrl = "untitled.glb", position = [0, 0, 0] }) {
-  const mesh = useRef(null);
-  const gltf = useLoader(GLTFLoader, fileUrl);
-
-  // Re-add the useFrame hook to rotate the object
-  useFrame(() => {
-    if (mesh.current) {
-      mesh.current.rotation.y += 0.01;
-    }
-  });
-
-  return (
-    <mesh ref={mesh}>
-      <primitive object={gltf.scene} position={position} />
     </mesh>
   );
 }
