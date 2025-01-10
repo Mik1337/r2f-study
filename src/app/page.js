a8ca15e537332749766db51f56a05a95761bee30
Mik <mik23@protonmail.ch>
2025-01-10 08:12:22 +0530

    added helpers / gizmos

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,15 @@
 "use client";
-import { ModelViewer } from "@/gltf";
 import { useRef, useEffect } from "react";
 import { Canvas, useFrame, useLoader } from "@react-three/fiber";
-import { OrbitControls } from "@react-three/drei";
+import {
+  GizmoHelper,
+  GizmoViewcube,
+  GizmoViewport,
+  OrbitControls,
+  useHelper,
+} from "@react-three/drei";
+import { SpotLightHelper } from "three";
+// import { useControls } from "leva";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 
 export default function Home() {
@@ -18,17 +25,62 @@ export default function Home() {
   return (
     <div className="flex bg-red-100  justify-center items-center h-screen">
       <Canvas>
-        <ambientLight position={[0, 0, 5]} intensity={10} />
-        <directionalLight position={[0, 0, 5]} intensity={10} />
-        <MeshComponent />
+        {/* Gizmo Wrapper */}
+        <GizmoHelper alignment="bottom-right" margin={[80, 80]}>
+          <GizmoViewport />
+          <GizmoViewcube />
+        </GizmoHelper>
+        <gridHelper args={[20, 20, 0xff22aa, 0x55ccff]} />
+        {/* <AnimatedBox /> */}
+        <TShirt />
+        <SpotLightWithHelper />
         <OrbitControls />
       </Canvas>
     </div>
   );
 }
 
-function MeshComponent() {
-  const fileUrl = "/untitled.glb";
+function AnimatedBox() {
+  const boxRef = useRef();
+
+  // let { speed } = useControls({
+  //   speed: {
+  //     value: 0.005,
+  //     min: 0.0,
+  //     max: 0.03,
+  //     step: 0.001,
+  //   },
+  // });
+
+  useFrame(() => {
+    // Animation Code
+    boxRef.current.rotation.y += 0.01;
+  });
+
+  return (
+    <mesh ref={boxRef}>
+      <boxGeometry args={[2, 3, 2]} />
+      <meshPhongMaterial color={0x00bfff} />
+    </mesh>
+  );
+}
+
+function SpotLightWithHelper() {
+  const light = useRef();
+
+  useHelper(light, SpotLightHelper, "orange");
+
+  return (
+    <spotLight
+      ref={light}
+      intensity={50}
+      position={[4, 2, 3]}
+      angle={Math.PI / 8}
+    />
+  );
+}
+
+function TShirt({ fileUrl = "untitled.glb", position = [0, 0, 0] }) {
   const mesh = useRef(null);
   const gltf = useLoader(GLTFLoader, fileUrl);
 
@@ -41,7 +93,7 @@ function MeshComponent() {
 
   return (
     <mesh ref={mesh}>
-      <primitive object={gltf.scene} position={[0, 0, 0]} />
+      <primitive object={gltf.scene} position={position} />
     </mesh>
   );
 }
